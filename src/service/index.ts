230abd1f5580c88721a80ee73dea18514c3cc3a6
Igor Oliveira dos Santos <igor.santos@hp.com>
2024-01-13 13:28:36 -0500

    test gtsolar call

--- src/service/index.ts
+++ src/service/index.ts
@@ -8,10 +8,10 @@ export const sendForm = async (inputs: Inputs) => {
     const data = await axios.post('/gtsolar', inputs)
     resolve(data.data)
   })
-  const responseSouEnergy = new Promise(async (resolve, _reject) => {
-    const data = await axios.post('/souenergy', inputs)
-    resolve(data.data)
-  })
+  // const responseSouEnergy = new Promise(async (resolve, _reject) => {
+  //   const data = await axios.post('/souenergy', inputs)
+  //   resolve(data.data)
+  // })
 
-  return Promise.all([responseGTSolar, responseSouEnergy])
+  return Promise.all([responseGTSolar])
 }
